f605b51fd5125ee63e0084a7a010edaed53e27fa
Wojciech <stawinskiwojciech@gmail.com>
2019-11-02 20:43:31 +0100

    Added HeroComponent and changed app prefix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ata-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
+})
+
+export class HeroComponent {
+  hero = 'Wojtek';
+}
