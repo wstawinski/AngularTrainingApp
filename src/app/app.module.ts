c20055795d959f4a18037febe8699095d2941cf0
Wojciech <stawinskiwojciech@gmail.com>
2019-11-11 21:20:33 +0100

    Cleaned modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
@@ -10,9 +7,6 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
     AppRoutingModule,
     PagesModule
   ],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from '../app-routing.module';
 
@@ -12,6 +13,7 @@ import { HeroDetailsComponent } from './hero-details/hero-details.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   exports: [
